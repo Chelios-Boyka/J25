cace6f8d2a91e231e6a5aedc4f24a0bfb13abf2f
ALAIN ITURRIA ABREGO <pinwi.94@gmail.com>
2026-01-05 00:03:41 -0600

    HTTP methods added

--- bancking-app/src/main/java/net/j25/bancking_app/controller/AccountController.java
+++ bancking-app/src/main/java/net/j25/bancking_app/controller/AccountController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
@@ -27,5 +30,43 @@ public class AccountController {
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
         AccountDto accountDto = accountService.getAccountById(id);
         return ResponseEntity.ok(accountDto);
+    }
+
+
+    //DEPOSIT REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> desposit(@PathVariable Long id,
+                                               @RequestBody Map<String, Double> request){
+
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(id, amount);
+
+        return ResponseEntity.ok(accountDto);
+    }
+
+    //WITHDRAW REST API
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id,
+                                               @RequestBody Map<String, Double> request){
+        double amount = request.get("amount");
+        AccountDto accountDto = accountService.withdraw(id, amount);
+
+        return ResponseEntity.ok(accountDto);
+    }
+
+    //GET ALL ACCOUNTS REST API
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAllAccounts(){
+
+        List<AccountDto> accounts = accountService.getAllAccounts();
+
+        return ResponseEntity.ok(accounts);
+    }
+
+    //DELETE ACCOUNT REST API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAccountById(@PathVariable Long id){
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok("Account deleted yey");
     }
 }
--- bancking-app/src/main/java/net/j25/bancking_app/service/AccountService.java
+++ bancking-app/src/main/java/net/j25/bancking_app/service/AccountService.java
@@ -2,9 +2,19 @@ package net.j25.bancking_app.service;
 
 import net.j25.bancking_app.dto.AccountDto;
 
+import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
 
     AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id, double amount);
+
+    AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccount(Long id);
 }
